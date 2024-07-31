e452d2af32876cfa4f660e11a54547ad2a4cf4f2
Angela Petrone <apetrone@hawk.iit.edu>
2024-07-31 09:34:54 -0500

    Create and export Body component

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Header from './components/Header'; 
+import Header from './components/Header'
+import NotesListPage from './pages/NotesListPage'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <NotesListPage />
     </div>
   );
 }
