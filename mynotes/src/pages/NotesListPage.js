a23d3e4f87e6bbc69197545c08e060b8b7c15ce6
Angela Petrone <apetrone@hawk.iit.edu>
2024-07-31 09:45:26 -0500

    Use note data and create child component for individual note

--- mynotes/src/pages/NotesListPage.js
+++ mynotes/src/pages/NotesListPage.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import notes from '../assets/data'
+import ListItem from '../components/Listitem'
 
 const NotesListPage = () => {
   return (
     <div>
-        Notes
+        <div className='notes-list'> 
+            {notes.map((note, index) => (
+                <ListItem key={index} note={note}/>
+            ))}
+        </div>
     </div>
   )
 }
