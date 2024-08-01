8c4d2f1f7aad66ac3ff5b01e9dc462151c8b866b
Angela Petrone <apetrone@hawk.iit.edu>
2024-07-31 20:01:57 -0500

    style the app

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -1,23 +1,21 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import Header from './components/Header'
-import NotesListPage from './pages/NotesListPage'
-import NotePage from './pages/NotePage'
+import "./App.css";
+import Header from "./components/Header";
+import NotesListPage from "./pages/NotesListPage";
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path='/' exact Component={NotesListPage} />
-          <Route path='/note/:id' Component={NotePage}/>
-        </Routes>
+      <div className="container dark">
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" exact Component={NotesListPage} />
+            <Route path="/note/:id" Component={NotePage} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
