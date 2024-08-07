fd665cbf277f5072b63ff191e94d3af1c6cb482a
Angela Petrone <apetrone@hawk.iit.edu>
2024-08-06 21:03:43 -0500

    Add update note method

--- mynotes/src/pages/NotePage.js
+++ mynotes/src/pages/NotePage.js
@@ -1,36 +1,60 @@
 import React, {useState, useEffect} from 'react'
 //import notes from '../assets/data'
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = () => {
     let {id} = useParams();
+    const navigate = useNavigate(); 
 
-    useEffect( ()=> {
+    useEffect(()=> {
         getNote()
-    }, [{id}])
+    }, [id])
+
+    let [ note, setNote] = useState([]);
 
     let getNote = async () => {
         let response = await fetch(`http://localhost:8000/notes/${id}`)
         let data = await response.json()
         setNote(data)
     }
-    let [note,setNote] = useState([]);
-    return (
-        <div className='note'>
-            <div className='note-header'>
-                <h3>
-                <Link to='/'> 
-                    <ArrowLeft />
-                </Link>
-                </h3>
-            </div>
-            <textarea value={note.body}>
 
-            </textarea>
+    let updateNote =  async() => {
+        await fetch(`http://localhost:8000/notes/${id}` , {
+            method:'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date() })
+        } )
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        navigate('/') // Use navigate for redirection
+    }
+
+    const handleChange = (e) => {
+        console.log("Textarea value:", e.target.value); // For debugging
+        setNote(prevNote => ({ ...prevNote, body: e.target.value }));
+    };
+
+    return (
+      <div className="note">
+        <div className="note-header">
+          <h3>
+            <Link to="/">
+              <ArrowLeft onClick={handleSubmit} />
+            </Link>
+          </h3>
         </div>
-    )
+        <textarea
+          onChange={handleChange}
+          placeholder="Edit note"
+          value={note.body || ""} // Ensure value is always a string
+        />
+      </div>
+    );
 }
 
 export default NotePage
